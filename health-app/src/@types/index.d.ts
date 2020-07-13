f073c1eacbea1677c770f9678e9bbbba83fcccc6
Matheus Filipe <mfcr031@gmail.com>
2020-07-13 10:18:40 -0300

    add emojis to app

--- health-app/src/@types/index.d.ts
+++ health-app/src/@types/index.d.ts
@@ -1,3 +1,10 @@
 declare module '*.png';
 
 declare module 'react-native-simple-alarm';
+
+declare module '*.svg' {
+  import { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
