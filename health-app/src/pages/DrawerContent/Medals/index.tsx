9483cb72a1e97d1f3ec986aedf82fc2665b74ccf
Matheus Filipe <mfcr031@gmail.com>
2020-09-03 21:44:26 -0300

    create medals page

--- health-app/src/pages/DrawerContent/Medals/index.tsx
+++ health-app/src/pages/DrawerContent/Medals/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import Header from '../components/Header';
+
+import { Container } from './styles';
 
 const Medals: React.FC = () => {
   return (
-    <View>
-      <Text>Medals</Text>
-    </View>
+    <Container>
+      <Header title="Medals" />
+    </Container>
   );
 };
 
